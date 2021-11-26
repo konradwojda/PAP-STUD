641c639ee898c51d09a136253de1c8e5f1fa8030
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-11-26 21:56:03 +0100

    add table column values and alive buttons in each section

--- src/main/java/edu/iipw/pap/Controller.java
+++ src/main/java/edu/iipw/pap/Controller.java
@@ -3,6 +3,9 @@ package edu.iipw.pap;
 import java.io.IOException;
 
 import edu.iipw.pap.db.model.Agency;
+import edu.iipw.pap.db.model.Line;
+import edu.iipw.pap.db.model.Stop;
+import edu.iipw.pap.db.model.WheelchairAccessibility;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -20,6 +24,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -63,49 +68,49 @@ public class Controller {
     private TableColumn<Agency, Integer> colAgencyId;
 
     @FXML
-    private TableColumn<?, ?> colAgencyName;
+    private TableColumn<Agency, String> colAgencyName;
 
     @FXML
-    private TableColumn<?, ?> colAgencyTelephone;
+    private TableColumn<Agency, String> colAgencyTelephone;
 
     @FXML
-    private TableColumn<?, ?> colAgencyTimezone;
+    private TableColumn<Agency, String> colAgencyTimezone;
 
     @FXML
-    private TableColumn<?, ?> colAgencyWebsite;
+    private TableColumn<Agency, String> colAgencyWebsite;
 
     @FXML
-    private TableColumn<?, ?> colLineAgencyId;
+    private TableColumn<Line, String> colLineAgencyId;
 
     @FXML
-    private TableColumn<?, ?> colLineCode;
+    private TableColumn<Line, String> colLineCode;
 
     @FXML
-    private TableColumn<?, ?> colLineDescription;
+    private TableColumn<Line, String> colLineDescription;
 
     @FXML
-    private TableColumn<?, ?> colLineId;
+    private TableColumn<Line, String> colLineId;
 
     @FXML
-    private TableColumn<?, ?> colLineType;
+    private TableColumn<Line, String> colLineType;
 
     @FXML
-    private TableColumn<?, ?> colStopCode;
+    private TableColumn<Stop, String> colStopCode;
 
     @FXML
-    private TableColumn<?, ?> colStopId;
+    private TableColumn<Stop, String> colStopId;
 
     @FXML
-    private TableColumn<?, ?> colStopLat;
+    private TableColumn<Stop, String> colStopLat;
 
     @FXML
-    private TableColumn<?, ?> colStopLon;
+    private TableColumn<Stop, String> colStopLon;
 
     @FXML
-    private TableColumn<?, ?> colStopName;
+    private TableColumn<Stop, String> colStopName;
 
     @FXML
-    private TableColumn<?, ?> colStopWheelchairAccesible;
+    private TableColumn<Stop, String> colStopWheelchairAccessible;
 
     @FXML
     private Pane plnStatus;
@@ -120,45 +125,40 @@ public class Controller {
     private GridPane pnStop;
 
     @FXML
-    private TableView<?> tblAgency;
+    private TableView<Agency> tblAgency;
 
     @FXML
-    private TableView<?> tblLine;
+    private TableView<Line> tblLine;
 
     @FXML
-    private TableView<?> tblStop;
+    private TableView<Stop> tblStop;
 
     @FXML
     private Text textHeader;
 
-    @FXML
-    void onAddAgency(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("addAgency.fxml"));
+    private void CreatePopUp(String fxml_template, Button button) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml_template));
         AnchorPane page = (AnchorPane) loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(page));
-        stage.initOwner(btnAddStop.getScene().getWindow());
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(button.getScene().getWindow());
         stage.showAndWait();
+    }
+
+    @FXML
+    void onAddAgency(ActionEvent event) throws IOException {
+        CreatePopUp("addAgency.fxml", btnAddAgency);
     }
 
     @FXML
     void onAddLine(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("addLine.fxml"));
-        AnchorPane page = (AnchorPane) loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(page));
-        stage.initOwner(btnAddStop.getScene().getWindow());
-        stage.showAndWait();
+        CreatePopUp("addLine.fxml", btnAddLine);
     }
 
     @FXML
     void onAddStop(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("addStop.fxml"));
-        AnchorPane page = (AnchorPane) loader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(page));
-        stage.initOwner(btnAddStop.getScene().getWindow());
-        stage.showAndWait();
+        CreatePopUp("addStop.fxml", btnAddStop);
     }
 
     @FXML
@@ -192,7 +192,14 @@ public class Controller {
 
     @FXML
     void onSearchAgency(ActionEvent event) {
+        colAgencyId.setCellValueFactory(new PropertyValueFactory<Agency, Integer>("agencyId"));
+        colAgencyName.setCellValueFactory(new PropertyValueFactory<Agency, String>("name"));
+        colAgencyWebsite.setCellValueFactory(new PropertyValueFactory<Agency, String>("website"));
+        colAgencyTimezone.setCellValueFactory(new PropertyValueFactory<Agency, String>("timezone"));
+        colAgencyTelephone.setCellValueFactory(new PropertyValueFactory<Agency, String>("telephone"));
 
+        Agency a1 = new Agency("MZK Stalowa Wola", "mzk.pl", "UTF+1", "123456789");
+        tblAgency.getItems().add(a1);
     }
 
     @FXML
@@ -202,6 +209,12 @@ public class Controller {
 
     @FXML
     void onSearchStop(ActionEvent event) {
+        // colStopId.setCellValueFactory(new PropertyValueFactory<Stop, Integer>("stopId"));
+        // colStopName.setCellValueFactory(new PropertyValueFactory<Stop, String>("name"));
+        // colStopCode.setCellValueFactory(new PropertyValueFactory<Stop, String>("code"));
+        // colStopLat.setCellValueFactory(new PropertyValueFactory<Stop, String>("lat"));
+        // colStopLon.setCellValueFactory(new PropertyValueFactory<Stop, String>("lon"));
+        // colStopWheelchairAccessible.setCellValueFactory(new PropertyValueFactory<Stop, WheelchairAccessibility>("wheelchairAccessible"));
 
     }
 
