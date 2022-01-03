6966c7b73963ce8c7a7ad306730eac647b986ab8
Mateusz Brzozowski <01158596@pw.edu.pl>
2022-01-03 12:28:00 +0100

    add main view files

---
+++ src/main/java/edu/iipw/pap/controller/ViewController.java
@@ -0,0 +1,23 @@
+package edu.iipw.pap.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+public class ViewController {
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private TableView<?> tblView;
+
+    @FXML
+    private Text txtHeader;
+}
