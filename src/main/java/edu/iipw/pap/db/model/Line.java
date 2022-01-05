8dc16d12b05860e59686a5332f1015cb62d4f2ea
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-05 14:10:47 +0100

    Model: Observable Line

--- src/main/java/edu/iipw/pap/db/model/Line.java
+++ src/main/java/edu/iipw/pap/db/model/Line.java
@@ -14,38 +14,125 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SetProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleSetProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 
 @Entity
 @Table(name = "lines")
-@NoArgsConstructor
-@Data
 public class Line {
-
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "line_id")
-    private int lineId;
+    private IntegerProperty lineId = new SimpleIntegerProperty();
+
+    public IntegerProperty lineIdProperty() {
+        return lineId;
+    }
+
+    public int getLineId() {
+        return lineId.get();
+    }
+
+    public void setLineId(int value) {
+        lineId.set(value);
+    }
 
     @Column(name = "code", nullable = false)
-    private String code;
+    private StringProperty code = new SimpleStringProperty();
+
+    public StringProperty codeProperty() {
+        return code;
+    }
+
+    public String getCode() {
+        return code.get();
+    }
+
+    public void setCode(String value) {
+        code.set(value);
+    }
 
     @Column(name = "description")
-    private String description;
+    private StringProperty description = new SimpleStringProperty();
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public void setDescription(String value) {
+        description.set(value);
+    }
 
     @Column(name = "type", nullable = false)
     @Enumerated(EnumType.ORDINAL)
-    private LineType type;
+    private ObjectProperty<LineType> type = new SimpleObjectProperty<LineType>();
+
+    public ObjectProperty<LineType> typeProperty() {
+        return type;
+    }
+
+    public LineType getType() {
+        return type.get();
+    }
+
+    public void setType(LineType value) {
+        type.set(value);
+    }
 
     @ManyToOne
     @JoinColumn(name = "agency_id", nullable = false)
-    private Agency agency;
+    private ObjectProperty<Agency> agency = new SimpleObjectProperty<Agency>();
+
+    public ObjectProperty<Agency> agencyProperty() {
+        return agency;
+    }
+
+    public Agency getAgency() {
+        return agency.get();
+    }
+
+    public void setAgency(Agency value) {
+        agency.set(value);
+    }
 
     @OneToMany(mappedBy = "line")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<Pattern> patterns;
+    private SetProperty<Pattern> patterns = new SimpleSetProperty<Pattern>();
+
+    public SetProperty<Pattern> patternsProperty() {
+        return patterns;
+    }
+
+    public ObservableSet<Pattern> getPatterns() {
+        return patterns.get();
+    }
+
+    public void setPatterns(Set<Pattern> value) {
+        if (ObservableSet.class.isAssignableFrom(value.getClass())) {
+            patterns.set((ObservableSet<Pattern>) value);
+        } else {
+            patterns.set(FXCollections.observableSet(value));
+        }
+    }
+
+    public String toString() {
+        return String.format(
+                "Line(%d, %s, description=%s, type=%s, agency=%s)",
+                getLineId(),
+                getCode(),
+                getDescription(),
+                getType(),
+                getAgency());
+    }
 }
