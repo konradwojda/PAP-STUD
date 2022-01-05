7413e5d041fec3330c9b51fc8928835845c3901f
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-05 15:25:56 +0100

    Model: Observable Stop

--- src/main/java/edu/iipw/pap/db/model/Stop.java
+++ src/main/java/edu/iipw/pap/db/model/Stop.java
@@ -12,40 +12,144 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SetProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleSetProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 
 @Entity
 @Table(name = "stops")
-@NoArgsConstructor
-@Data
 public class Stop {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "stop_id")
-    private int stopId;
+    private IntegerProperty stopId = new SimpleIntegerProperty();
+
+    public IntegerProperty stopIdProperty() {
+        return stopId;
+    }
+
+    public int getStopId() {
+        return stopId.get();
+    }
+
+    public void setStopId(int value) {
+        stopId.set(value);
+    }
 
     @Column(name = "name", nullable = false)
-    private String name;
+    private StringProperty name = new SimpleStringProperty();
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String value) {
+        name.set(value);
+    }
 
     @Column(name = "code")
-    private String code;
+    private StringProperty code = new SimpleStringProperty();
+
+    public StringProperty codeProperty() {
+        return code;
+    }
+
+    public String getCode() {
+        return code.get();
+    }
+
+    public void setCode(String value) {
+        code.set(value);
+    }
 
     @Column(name = "lat", nullable = false)
-    private double lat;
+    private DoubleProperty lat = new SimpleDoubleProperty();
+
+    public DoubleProperty latProperty() {
+        return lat;
+    }
+
+    public double getLat() {
+        return lat.get();
+    }
+
+    public void setLat(double value) {
+        lat.set(value);
+    }
 
     @Column(name = "lon", nullable = false)
-    private double lon;
+    private DoubleProperty lon = new SimpleDoubleProperty();
+
+    public DoubleProperty lonProperty() {
+        return lon;
+    }
+
+    public double getLon() {
+        return lon.get();
+    }
+
+    public void setLon(double value) {
+        lon.set(value);
+    }
 
     @Column(name = "wheelchair_accessible", nullable = false)
     @Enumerated(EnumType.ORDINAL)
-    private WheelchairAccessibility wheelchairAccessible = WheelchairAccessibility.UNKNOWN;
+    private ObjectProperty<WheelchairAccessibility> wheelchairAccessible = new SimpleObjectProperty<WheelchairAccessibility>(
+            WheelchairAccessibility.UNKNOWN);
+
+    public ObjectProperty<WheelchairAccessibility> wheelchairAccessibleProperty() {
+        return wheelchairAccessible;
+    }
+
+    public WheelchairAccessibility getWheelchairAccessible() {
+        return wheelchairAccessible.get();
+    }
+
+    public void setWheelchairAccessible(WheelchairAccessibility value) {
+        wheelchairAccessible.set(value);
+    }
 
     @OneToMany(mappedBy = "stop")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<PatternStop> patternStops;
+    private SetProperty<PatternStop> patternStops = new SimpleSetProperty<PatternStop>();
+
+    public SetProperty<PatternStop> patternStopsProperty() {
+        return patternStops;
+    }
+
+    public ObservableSet<PatternStop> getPatternStops() {
+        return patternStops.get();
+    }
+
+    public void setPatternStops(Set<PatternStop> value) {
+        if (ObservableSet.class.isAssignableFrom(value.getClass())) {
+            patternStops.set((ObservableSet<PatternStop>) value);
+        } else {
+            patternStops.set(FXCollections.observableSet(value));
+        }
+    }
+
+    public String toString() {
+        return String.format(
+                "Stop(%d, %s, code=%s, lat=%f, lon=%f, wheelchairAccessible=%s)",
+                getStopId(),
+                getName(),
+                getCode(),
+                getLat(),
+                getLon(),
+                getWheelchairAccessible());
+    }
 }
