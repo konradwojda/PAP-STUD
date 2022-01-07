5eae274a5b7e5db6d21a5e1a6195fe89791a0a0b
Konrad Wojda <konrad2001@outlook.com>
2022-01-07 12:41:58 +0100

    Started to implement stop timetable

--- src/main/java/edu/iipw/pap/controller/ViewStopTimetableController.java
+++ src/main/java/edu/iipw/pap/controller/ViewStopTimetableController.java
@@ -1,11 +1,17 @@
 package edu.iipw.pap.controller;
 
+import java.util.List;
+
+import edu.iipw.pap.db.Database;
 import edu.iipw.pap.db.model.Calendar;
+import edu.iipw.pap.db.model.Line;
 import edu.iipw.pap.db.model.Stop;
+import edu.iipw.pap.db.model.StopTime;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 public class ViewStopTimetableController {
@@ -16,26 +22,39 @@ public class ViewStopTimetableController {
     private ChoiceBox<Stop> choiceStop;
 
     @FXML
-    private TableColumn<?, ?> colDeparture;
+    private TableColumn<StopTime, Integer> colDeparture;
 
     @FXML
-    private TableColumn<?, ?> colHeadsign;
+    private TableColumn<StopTime, String> colHeadsign;
 
     @FXML
-    private TableColumn<?, ?> colLine;
+    private TableColumn<StopTime, Line> colLine;
 
     @FXML
     private VBox listLine;
 
     @FXML
-    private TableView<?> tblStopTimetable;
+    private TableView<StopTime> tblStopTimetable;
 
     private void refreshStopTimetableTable() {
-        // TODO: fill
+        try {
+            Calendar chosenCalendar = choiceCalendar.getValue();
+            Stop chosenStop = choiceStop.getValue();
+            this.tblStopTimetable.getItems().setAll((StopTime[])chosenStop.getStopTimes().filter(s -> s.getTrip().getCalendar() == chosenCalendar).toArray());
+        }
+        catch (Exception e)
+        {
+        }
     }
 
     public void InitializeStopTimetableTable() {
-        // TODO: fill
+        choiceCalendar.getItems().setAll(Database.INSTANCE.listAll(Calendar.class));
+        choiceStop.getItems().setAll(Database.INSTANCE.listAll(Stop.class));
+
+        colDeparture.setCellValueFactory(new PropertyValueFactory<StopTime, Integer>("departureTime"));
+        colLine.setCellValueFactory(new PropertyValueFactory<StopTime, Line>("trip.getPattern().getLine()"));
+        colHeadsign.setCellValueFactory(new PropertyValueFactory<StopTime, String>("trip.getPattern().getHeadsign()"));
+
         refreshStopTimetableTable();
     }
 
