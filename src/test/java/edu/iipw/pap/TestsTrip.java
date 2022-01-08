20ae87fdd4e884b8f4046c47b629fa10c3b28d47
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-08 14:40:35 +0100

    Test trip.getStopTimes()

--- src/test/java/edu/iipw/pap/TestsTrip.java
+++ src/test/java/edu/iipw/pap/TestsTrip.java
@@ -1,10 +1,12 @@
 package edu.iipw.pap;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import edu.iipw.pap.db.model.Line;
 import edu.iipw.pap.db.model.Pattern;
 import edu.iipw.pap.db.model.Trip;
 import edu.iipw.pap.exceptions.InvalidData;
@@ -39,5 +41,17 @@ public class TestsTrip {
         // Validate wheelchair-accessibility checking
         t.setWheelchairAccessible(null);
         assertThrows(InvalidData.class, () -> t.validateUserInput());
+    }
+
+    @Test
+    void checkStopTimes() {
+        // Every trip should have precisely 3 stop times
+        for (Line l : Dataset.INSTANCE.listAll(Line.class)) {
+            for (Pattern p : l.patternsProperty()) {
+                for (Trip t : p.tripsProperty()) {
+                    assertEquals(3, t.getStopTimes().count());
+                }
+            }
+        }
     }
 }
