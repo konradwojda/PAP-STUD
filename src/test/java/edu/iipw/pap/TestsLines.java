1949cc5c78d1ea1ce2796c2d310764d5b8ea3917
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-08 12:48:47 +0100

    Test Line.validateUserInput()

--- src/test/java/edu/iipw/pap/TestsLines.java
+++ src/test/java/edu/iipw/pap/TestsLines.java
@@ -1,10 +1,16 @@
 package edu.iipw.pap;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import edu.iipw.pap.db.model.Line;
+import edu.iipw.pap.db.model.LineType;
+import edu.iipw.pap.db.model.Pattern;
+import edu.iipw.pap.exceptions.InvalidData;
+import javafx.collections.FXCollections;
 
 public class TestsLines {
     @Test
@@ -12,5 +18,38 @@ public class TestsLines {
         assertEquals(2, Dataset.INSTANCE.listAll(Line.class).size());
         assertEquals(2, Dataset.INSTANCE.lineM1.patternsProperty().size());
         assertEquals(2, Dataset.INSTANCE.lineM2.patternsProperty().size());
+    }
+
+    @Test
+    void checkValidateUserInput() {
+        Line l = new Line();
+        l.setCode("M1");
+        l.setDescription("Kabaty - Młociny");
+        l.setType(LineType.METRO);
+        l.setAgency(Dataset.INSTANCE.agency);
+
+        // Check against a valid Line
+        assertDoesNotThrow(() -> l.validateUserInput());
+
+        // Validate agency-checking
+        l.setAgency(null);
+        assertThrows(InvalidData.class, () -> l.validateUserInput());
+        l.setAgency(Dataset.INSTANCE.agency);
+
+        // Validate code-checking
+        l.setCode(null);
+        assertThrows(InvalidData.class, () -> l.validateUserInput());
+        l.setCode("");
+        assertThrows(InvalidData.class, () -> l.validateUserInput());
+        l.setCode("M1");
+
+        // Validate type-checking
+        l.setType(null);
+        assertThrows(InvalidData.class, () -> l.validateUserInput());
+        l.setType(LineType.METRO);
+
+        // Validate recursion into patterns
+        l.setPatterns(FXCollections.observableSet(new Pattern()));
+        assertThrows(InvalidData.class, () -> l.validateUserInput());
     }
 }
