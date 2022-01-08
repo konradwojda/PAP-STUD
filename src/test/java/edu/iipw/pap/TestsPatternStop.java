1e2ef77c05b007958cf40a57607170977b335f20
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-08 13:59:54 +0100

    Test PatternStop.validateUserInput

---
+++ src/test/java/edu/iipw/pap/TestsPatternStop.java
@@ -0,0 +1,42 @@
+package edu.iipw.pap;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import edu.iipw.pap.db.model.Pattern;
+import edu.iipw.pap.db.model.PatternStop;
+import edu.iipw.pap.exceptions.InvalidData;
+
+public class TestsPatternStop {
+   @Test
+   void checkValidateUserInput() {
+        Pattern p = new Pattern();
+        PatternStop ps = new PatternStop();
+        ps.setPattern(p);
+        ps.setStop(Dataset.INSTANCE.stopKabaty);
+
+        // Check against a valid PatternStop
+        assertDoesNotThrow(() -> ps.validateUserInput());
+
+        // Validate stop checking
+        ps.setStop(null);
+        assertThrows(InvalidData.class, () -> ps.validateUserInput());
+        ps.setStop(Dataset.INSTANCE.stopKabaty);
+
+        // Validate pattern checking
+        ps.setPattern(null);
+        assertThrows(InvalidData.class, () -> ps.validateUserInput());
+        ps.setPattern(p);
+
+        // Validate index checking
+        ps.setIndex(-1);
+        assertThrows(InvalidData.class, () -> ps.validateUserInput());
+        ps.setIndex(0);
+
+        // Validate travel time checking
+        ps.setTravelTime(-1);
+        assertThrows(InvalidData.class, () -> ps.validateUserInput());
+   }
+}
