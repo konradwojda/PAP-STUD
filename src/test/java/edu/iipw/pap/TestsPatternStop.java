f4d3d6481dab13d2fbf34127f198ecf9632f5027
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-01-08 14:14:37 +0100

    Test PatternStop.getStopTimes()

--- src/test/java/edu/iipw/pap/TestsPatternStop.java
+++ src/test/java/edu/iipw/pap/TestsPatternStop.java
@@ -1,12 +1,14 @@
 package edu.iipw.pap;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import edu.iipw.pap.db.model.Pattern;
 import edu.iipw.pap.db.model.PatternStop;
+import edu.iipw.pap.db.model.Stop;
 import edu.iipw.pap.exceptions.InvalidData;
 
 public class TestsPatternStop {
@@ -38,5 +40,14 @@ public class TestsPatternStop {
         // Validate travel time checking
         ps.setTravelTime(-1);
         assertThrows(InvalidData.class, () -> ps.validateUserInput());
+   }
+
+   @Test
+   void checkStopTimes() {
+      // Every pattern stop should have 10 stop times
+      for (Stop s : Dataset.INSTANCE.listAll(Stop.class)) {
+         for (PatternStop ps : s.patternStopsProperty())
+            assertEquals(10, ps.getStopTimes().count());
+      }
    }
 }
